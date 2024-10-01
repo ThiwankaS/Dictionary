702c633117a8490b6a7350bd90011da4d3dee0e5
Thiwanka Somachandra <thivankas@outlook.com>
2024-10-01 04:21:36 +0300

    Remove unnessary

--- includes/ft_number.h
+++ includes/ft_number.h
@@ -5,7 +5,7 @@
 
 #define END 0
 #define MID 1
-#define BUFFER_SIZE 41
+#define BUFFER_SIZE 40
 
 typedef struct NumeralsData
 {
@@ -23,16 +23,10 @@ typedef struct Digit
     int book_mark;
 } t_digit;
 
-int ft_strlen(char *str);
+char *ft_only_alpha(char *str);
+char *ft_only_number(char *str);
+
 t_num_data *loading_data(FILE *directory);
 t_digit *loading_digits(char *input_number);
-char *final_numeral(t_num_data *data, t_digit *digits, char *result);
-char *ft_number_string(char c1, char c2, int size);
-char *get_prefix(t_num_data *data, char nbr, int length);
-char *get_range(t_num_data *data, char nbr, int length);
-int ft_strcmp(char *s1, char *s2);
-char *get_two_digit(t_num_data *data, t_digit *digits, char nbr, int *index);
-char *get_last_digit(t_num_data *data, t_digit *digits, char nbr, int *index);
-char *three_digit_number(t_num_data *data, t_digit *digits, char *result);
 
 #endif
